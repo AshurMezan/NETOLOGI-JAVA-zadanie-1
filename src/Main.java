389aee0afcf93fab6468276afe6607a16a324e10
AshurMezan <102794363+AshurMezan@users.noreply.github.com>
2022-12-09 19:02:07 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 100; // баланс у клиента 80 рублей
-        int b = 1500; // Сумма пополнения на баланс
-        int c; // Сумма бонуса
+        int currentBalance = 100;
+        int replenishment = 1500;
+        int bonus = 0;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        }
+        int endBalance = currentBalance + replenishment + bonus;
 
-        if (b < 1000) {c = 0;}
-        else {c = b / 100;}
-
-        int d = a + b + c; // Итоговый баланс клиента
-
-        System.out.println(d);
+        System.out.println(endBalance + " количество бонусных рублей: " + bonus);
     }
 }
